8f1eda69cb757ea8666189b82cc0ac9c5f8e4ca1
Marco Retamoso <retamosorodriguezmarcoantonio@gmail.com>
2025-12-19 21:37:11 -0400

    install prime vuejs and tailwind

---
+++ resources/js/Pages/AppLayout.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <div class="p-6">
+    <Button label="Guardar" icon="pi pi-check" />
+  </div>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
-import './bootstrap';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+import PrimeVue from 'primevue/config'
+
+// CSS de PrimeVue
+import 'primeicons/primeicons.css'
+
+// Tema (elige uno)
+import Aura from '@primevue/themes/aura'
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}.vue`),
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(PrimeVue, {
+        theme: {
+          preset: Aura
+        }
+      })
+      .mount(el)
+  },
+})
